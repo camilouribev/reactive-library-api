9ebd10cf5f030dce8dc72d2de616c39c6c1046a8
Camilo U <cauribeva@unal.edu.co>
2021-10-18 18:53:12 -0500

    Consolidated routers and usecases in single classes

--- src/main/java/com/sofka/newreactivelibrary/routers/ResourceRouters.java
+++ src/main/java/com/sofka/newreactivelibrary/routers/ResourceRouters.java
@@ -9,12 +9,24 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class GetAllResourcesRouter {
+public class ResourceRouters {
+
+    @Bean
+    public RouterFunction<ServerResponse> createResource(ResourceUseCases resourceUseCases) {
+        return route(POST("/api/new").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(ResourceDTO.class)
+                        .flatMap(resDTO -> resourceUseCases.createResource(resDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                        )
+        );
+    }
+
     @Bean
     public RouterFunction<ServerResponse> getAll(ResourceUseCases resourceUseCases) {
         return route(
@@ -24,4 +36,6 @@ public class GetAllResourcesRouter {
                         .body(BodyInserters.fromPublisher(resourceUseCases.getAllResources(), ResourceDTO.class))
         );
     }
+
+
 }
